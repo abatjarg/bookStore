f258b5c9a20f963c8303393cd2ce6117cf795fbd
Ariunjargal Batjargal <abatjarg@umass.edu>
2014-07-23 23:04:36 -0400

    Started implementing the server side

--- public/controllers/bookStoreController.js
+++ public/controllers/bookStoreController.js
@@ -1,11 +1,13 @@
 angular.module("bookStore")
-    .controller('bookStoreCtrl', function($scope){
-        $scope.data = {
-            books: [
-                {title: "Learn Angularjs", author: "Aj Batja", category: "angularjs", price: 100},
-                {title: "Learn Angularjs1", author: "Aj Batja", category: "angularjs1", price: 110},
-                {title: "Learn Angularjs2", author: "Aj Batja", category: "angularjs2", price: 200},
-                {title: "Learn Angularjs3", author: "Aj Batja", category: "angularjs3", price: 400}
-            ]
-        };
+    .controller('bookStoreCtrl', function($scope, $http){
+        $scope.data = {};
+
+        $http.get('/api/books')
+            .success(function(data){
+                $scope.data.books = data;
+            })
+            .error(function(error){
+                $scope.data.error = error;
+            });
+
     });
--- server.js
+++ server.js
@@ -3,6 +3,18 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var bookSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    category: String,
+    price: Number
+});
+
+var Book = mongoose.model('Book', bookSchema);
+
+mongoose.connect('mongodb://localhost/bookDatabase');
 
 var app = express();
 
@@ -12,6 +24,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/books', function(req, res, next){
+    var query = Book.find();
+    query.exec(function(err, books){
+        if(err) return next(err);
+        res.send(books);
+    });
+});
 
 app.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
